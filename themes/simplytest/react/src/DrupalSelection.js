8469a5ee67e0ee01d63b4263aecfdbf78037d25b
sumitmadan <sumitmadan@1538790.no-reply.drupal.org>
2019-07-18 15:16:00 -0400

    Issue #3063304 by sumitmadan, nerdstein: Pull projects and versions when user types project name

--- themes/simplytest/react/src/DrupalSelection.js
+++ themes/simplytest/react/src/DrupalSelection.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import DrupalCoreSelector from './DrupalCoreSelector';
+import DrupalProjectSelector from './DrupalProjectSelector';
 
 class DrupalSelection extends React.Component {
     render() {
         return (
             <div className="drupal-selection">
                 <div className="drupal-core-selection">
-                    <DrupalCoreSelector />
+                    <DrupalProjectSelector />
                 </div>
                 <button value="yes">Select</button>
             </div>
