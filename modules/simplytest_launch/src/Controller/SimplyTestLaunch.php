25fc6c781d19da9d6b481be71194806cdda02763
Matt Glaman <nmd.matt@gmail.com>
2020-12-26 13:53:29 -0600

    Create a landing page

--- modules/simplytest_launch/src/Controller/SimplyTestLaunch.php
+++ modules/simplytest_launch/src/Controller/SimplyTestLaunch.php
@@ -3,25 +3,19 @@
 namespace Drupal\simplytest_launch\Controller;
 
 use Drupal\Component\Serialization\Json;
-use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Render\Markup;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\simplytest_projects\SimplytestProjectFetcher;
 use Drupal\simplytest_tugboat\InstanceManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for config module routes.
  */
-class SimplyTestLaunch extends ControllerBase implements ContainerInjectionInterface {
-
-  /**
-   * The request object.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  protected $requestStack;
+class SimplyTestLaunch implements ContainerInjectionInterface {
 
   /**
    * Simplytest Project Fetcher Service.
@@ -47,8 +41,7 @@ class SimplyTestLaunch extends ControllerBase implements ContainerInjectionInter
    * @param \Drupal\simplytest_tugboat\InstanceManagerInterface
    *   The simplytest tugboat instance manager service.
    */
-  public function __construct(RequestStack $requestStack, SimplytestProjectFetcher $simplytest_project_fetcher, InstanceManagerInterface $instance_manager) {
-    $this->requestStack = $requestStack;
+  public function __construct(SimplytestProjectFetcher $simplytest_project_fetcher, InstanceManagerInterface $instance_manager) {
     $this->simplytestProjectFetcher = $simplytest_project_fetcher;
     $this->instanceManager = $instance_manager;
   }
@@ -58,10 +51,73 @@ class SimplyTestLaunch extends ControllerBase implements ContainerInjectionInter
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('request_stack'),
       $container->get('simplytest_projects.fetcher'),
       $container->get('simplytest_tugboat.instance_manager')
     );
+  }
+
+  public function configure(Request $request) {
+    $text = [
+      'problems' => [
+        'title' => new TranslatableMarkup('Encountered a problem?'),
+        'text' => new TranslatableMarkup('Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue.'),
+        'link' => [
+          '#type' => 'link',
+          '#title' => new TranslatableMarkup('Report a problem'),
+          '#url' => 'https://www.drupal.org/project/issues/simplytest',
+        ]
+      ],
+      'future' => [
+        'title' => new TranslatableMarkup('Future of Simplytest.me'),
+        'text' => new TranslatableMarkup('Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue.'),
+        'link' => [
+          '#type' => 'link',
+          '#title' => new TranslatableMarkup('Join the conversation'),
+          '#url' => 'https://www.drupal.org/project/issues/simplytest',
+        ]
+      ],
+      'spread' => [
+        'title' => new TranslatableMarkup('Spread the word'),
+        'text' => new TranslatableMarkup('Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue.'),
+        'link' => [
+          '#type' => 'link',
+          '#title' => new TranslatableMarkup('Follow'),
+          '#url' => 'https://www.drupal.org/project/issues/simplytest',
+        ]
+      ],
+    ];
+    $build = [
+      'mount' => [
+        '#markup' => Markup::create('<div class="simplytest-react-component" id="root"></div>'),
+        '#attached' => [
+          'library' => [
+            'simplytest_theme/react',
+          ],
+          'drupalSettings' => [
+            // Pass custom launcher values to drupalSettings.
+            'launcher' => $request->query->get('launcher', []),
+          ],
+        ],
+      ],
+      'triptych' => [
+        '#type' => 'container',
+        '#attributes' => [
+          'class' => 'triptych',
+        ],
+      ]
+    ];
+    foreach ($text as $key => $content) {
+      $build['triptych'][$key] = [
+        '#type' => 'inline_template',
+        '#template' => '<div class="col block">
+    <h2 class="block__title">{{title}}</h2>
+        <div><p>{{text}}</p></div>
+        <div><a href="/">Report a problem</a></div>
+</div>',
+        '#context' => $content,
+      ];
+    }
+    return $build;
   }
 
   /**
@@ -74,8 +130,8 @@ class SimplyTestLaunch extends ControllerBase implements ContainerInjectionInter
   /**
    * Project launcher service for react.
    */
-  public function launchProject() {
-    $submission = Json::decode($this->requestStack->getCurrentRequest()->getContent());
+  public function launchProject(Request $request) {
+    $submission = Json::decode($request->getContent());
 
     // @todo add more validation when ocd button is clicked.
     if ($error = $this->validateSubmission($submission)) {
