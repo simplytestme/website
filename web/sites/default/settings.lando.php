d9fcc64fe65d69e6259c7514e30af6d3956f4af6
Matthew Obert <matt@hotwebmatter.com>
2021-02-20 18:24:49 -0500

    Remove files directory paths from Lando settings.

--- web/sites/default/settings.lando.php
+++ web/sites/default/settings.lando.php
@@ -11,10 +11,6 @@
 if (getenv('LANDO_INFO')) {
   $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
 
-  // Files directory paths.
-  $settings['file_public_path'] = 'sites/default/files';
-  $settings['file_private_path'] = 'sites/default/files/private';
-
   // Allow any domains to access the site with Lando.
   $settings['trusted_host_patterns'] = [
     '^(.+)$',
