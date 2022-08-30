98769526cf8e9d5ec5dfa0336210a477cbbeff69
nerdstein <nerdstein@1557710.no-reply.drupal.org>
2022-08-30 16:45:40 +0000

    Added no-interaction to composer plugin

--- modules/simplytest_tugboat/src/PreviewConfigGenerator.php
+++ modules/simplytest_tugboat/src/PreviewConfigGenerator.php
@@ -306,7 +306,7 @@ final class PreviewConfigGenerator {
         $composerWorkingDir = $parameters['major_version'] !== '8' ? 'stm' : '"${DOCROOT}"';
         $commands[] = 'composer global require szeidler/composer-patches-cli:~1.0';
         $commands[] = 'cd ' . $composerWorkingDir .  ' && composer require cweagans/composer-patches:~1.0 --no-update';
-        $commands[] = 'composer config --no-plugins allow-plugins.cweagans/composer-patches true';
+        $commands[] = 'composer config --no-interaction allow-plugins.cweagans/composer-patches true';
         $commands[] = 'cd ' . $composerWorkingDir .  ' && composer patch-enable --file="patches.json"';
         foreach ($parameters['patches'] as $patch) {
           $commands[] = $this->getComposerPatchCommand($parameters['project'], $patch, $composerWorkingDir);
