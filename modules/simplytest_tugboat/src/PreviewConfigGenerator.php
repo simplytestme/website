9adcff05b2116c5d2e011b39be2bff8ecafe8c73
Adam Bergstein <nerdstein@gmail.com>
2022-09-09 20:16:25 -0400

    3308715 - more bug fixing

--- modules/simplytest_tugboat/src/PreviewConfigGenerator.php
+++ modules/simplytest_tugboat/src/PreviewConfigGenerator.php
@@ -308,7 +308,7 @@ final class PreviewConfigGenerator {
         $composerWorkingDir = $parameters['major_version'] !== '8' ? 'stm' : '"${DOCROOT}"';
         $commands[] = 'composer global require szeidler/composer-patches-cli:~1.0 --no-update';
         $commands[] = 'cd ' . $composerWorkingDir .  ' && composer require cweagans/composer-patches:~1.0 --no-update';
-        $commands[] = 'composer config --no-interaction allow-plugins.cweagans/composer-patches true';
+        $commands[] = 'composer global config --no-interaction allow-plugins.cweagans/composer-patches true';
         $commands[] = 'cd ' . $composerWorkingDir .  ' && composer patch-enable --file="patches.json"';
         foreach ($parameters['patches'] as $patch) {
           $commands[] = $this->getComposerPatchCommand($parameters['project'], $patch, $composerWorkingDir);
