2cb257b455906f68fe2ceb3f352bfd4994b26a06
Matt Glaman <nmd.matt@gmail.com>
2024-08-14 14:53:33 -0500

    remove gulp-stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cleanCSS = require('gulp-clean-css'),
   plumber = require('gulp-plumber'),
-  gulpStylelint = require('gulp-stylelint'),
   mixin = require('postcss-mixins')
 
 // Variables for folder path.
@@ -21,39 +20,6 @@ var paths = {
   }
 };
 
-// Lint CSS files.
-gulp.task('lint:css', function () {
-  return gulp.src(paths.styles.source + '/*.css')
-    .pipe(plumber())
-    .pipe(gulpStylelint({
-      reporters: [{
-        formatter: 'string',
-        console: true
-      }]
-    }))
-    .pipe(plumber.stop());
-});
-
-// Lint CSS files and throw an error for a CI to catch.
-gulp.task('lint:css-with-fail', function () {
-  return gulp.src(paths.styles.source + '/*.css')
-    .pipe(plugins.gulpStylelint({
-      reporters: [{
-        formatter: 'string',
-        console: true,
-        failAfterError: true
-      }]
-    }));
-});
-
-// Fix CSS linting errors.
-gulp.task('lint:css-fix', function () {
-  return gulp.src(paths.styles.source + '/*.css')
-    .pipe(gulpStylelint({
-      fix: true
-    }))
-    .pipe(gulp.dest(paths.styles.source));
-});
 
 // Build CSS files.
 gulp.task('build:css', function () {
@@ -87,9 +53,4 @@ gulp.task('build:css', function () {
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.destination))
-});
-
-// Watch CSS.
-gulp.task('watch:css', function () {
-  gulp.watch(paths.styles.source, gulp.series('build:css'));
 });
