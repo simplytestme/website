079a96b4d30a228ff1e11ae395dbd6f459ff4ffb
Matt Glaman <nmd.matt@gmail.com>
2025-08-17 10:25:46 -0500

    Fix Commerce Kickstart demo (#502)

--- web/modules/custom/simplytest_ocd/src/Plugin/OneClickDemo/Commerce.php
+++ web/modules/custom/simplytest_ocd/src/Plugin/OneClickDemo/Commerce.php
@@ -5,12 +5,9 @@ namespace Drupal\simplytest_ocd\Plugin\OneClickDemo;
 /**
  * Provides one click demo for commerce.
  *
- * This directly extends OneClickDemoBase since it has its own Composer template
- * for the demo.
- *
  * @OneClickDemo(
  *   id = "oneclickdemo_commerce",
- *   title = @Translation("Drupal Commerce Demo"),
+ *   title = @Translation("Commerce Kickstart Demo"),
  *   base_preview_name = "commerce"
  * )
  */
@@ -21,6 +18,7 @@ class Commerce extends OneClickDemoBase {
     return [
       'docker-php-ext-install opcache',
       'docker-php-ext-install bcmath',
+      'echo "memory_limit = 512M" >> /usr/local/etc/php/conf.d/my-php.ini',
       'a2enmod headers rewrite',
       'rm -rf "${DOCROOT}"',
     ];
@@ -29,15 +27,12 @@ class Commerce extends OneClickDemoBase {
   #[\Override]
   public function getDownloadCommands(array $parameters): array {
     $commands = [
-      // @todo the base preview doesn't have the `commerce` dir?
       'cd "${TUGBOAT_ROOT}" && composer create-project centarro/commerce-kickstart-project stm --no-install --stability dev --no-interaction',
-      'cd "${TUGBOAT_ROOT}/stm" && composer require --no-update drupal/commerce_demo:^3.0',
       // Remove bin-dir customization,
       'cd "${TUGBOAT_ROOT}/stm" && composer config bin-dir --unset',
       'cd "${TUGBOAT_ROOT}/stm" && composer install',
       'ln -snf "${TUGBOAT_ROOT}/stm/web" "${DOCROOT}"'
     ];
-    // $commands[] = 'cd "${TUGBOAT_ROOT}"/commerce && composer update --no-ansi';
     return $commands;
   }
 
@@ -51,7 +46,6 @@ class Commerce extends OneClickDemoBase {
     $commands = [];
     $commands[] = 'echo \'$settings["file_private_path"] = "sites/default/files/private";\' >> ${DOCROOT}/sites/default/settings.php';
     $commands[] = 'cd "${DOCROOT}" && ../vendor/bin/drush si --db-url=mysql://tugboat:tugboat@mysql:3306/tugboat --account-name=admin --account-pass=admin -y';
-    $commands[] = 'cd "${DOCROOT}" && ../vendor/bin/drush en commerce_demo -y';
     return $commands;
   }
 
