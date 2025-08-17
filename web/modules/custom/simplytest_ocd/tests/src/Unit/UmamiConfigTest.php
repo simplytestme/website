5f6b27a11570b3d49e3a4ebd087f29a871d2a976
Matt Glaman <nmd.matt@gmail.com>
2025-08-17 09:05:41 -0500

    Do not invoke Drush using PHP
    Drush changed its script from a PHP script to shell.

--- web/modules/custom/simplytest_ocd/tests/src/Unit/UmamiConfigTest.php
+++ web/modules/custom/simplytest_ocd/tests/src/Unit/UmamiConfigTest.php
@@ -30,7 +30,7 @@ final class UmamiConfigTest extends OneClickDemoConfigTestBase {
             'cd stm && composer update --no-ansi',
             'echo "SIMPLYEST_STAGE_INSTALLING"',
             'cd "${DOCROOT}" && chmod -R 777 sites/default',
-            'cd ${DOCROOT} && php -d memory_limit=-1 ../vendor/bin/drush si demo_umami --db-url=mysql://tugboat:tugboat@mysql:3306/tugboat --account-name=admin --account-pass=admin -y',
+            'cd ${DOCROOT} && ../vendor/bin/drush si demo_umami --db-url=mysql://tugboat:tugboat@mysql:3306/tugboat --account-name=admin --account-pass=admin -y',
             'cd "${DOCROOT}" && ../vendor/bin/drush config-set system.logging error_level verbose -y',
             'chown -R www-data:www-data "${DOCROOT}"/sites/default/files',
             'echo "SIMPLYEST_STAGE_FINALIZE"',
