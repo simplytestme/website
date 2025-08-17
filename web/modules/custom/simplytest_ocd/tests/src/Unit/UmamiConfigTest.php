7d90374283211af73db6b2be805069b43720237c
Matt Glaman <nmd.matt@gmail.com>
2025-08-17 09:42:04 -0500

    Set memory_limit for Umami demo

--- web/modules/custom/simplytest_ocd/tests/src/Unit/UmamiConfigTest.php
+++ web/modules/custom/simplytest_ocd/tests/src/Unit/UmamiConfigTest.php
@@ -20,9 +20,10 @@ final class UmamiConfigTest extends OneClickDemoConfigTestBase {
           'build' => [
             'composer self-update',
             'docker-php-ext-install opcache',
+            'echo "memory_limit = 512M" >> /usr/local/etc/php/conf.d/my-php.ini',
             'a2enmod headers rewrite',
             'rm -rf "${DOCROOT}"',
-            'composer -n create-project drupal/recommended-project:^10 stm --no-install',
+            'composer -n create-project drupal/recommended-project stm --no-install',
             'cd stm && composer require --no-update drush/drush',
             'ln -snf "${TUGBOAT_ROOT}/stm/web" "${DOCROOT}"',
             'echo "SIMPLYEST_STAGE_DOWNLOAD"',
